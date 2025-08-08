d665d0fff378f8949044e6f82ff41219c1152c56
Cuantocos <santiagoortiz0609@hotmail.com>
2025-08-08 02:48:24 +0200

    Agregue la funcion async de insertar usuario

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,11 @@
-import pool from "../config/db.js"
+import {pool} from "../config/db.js"
+
+export async function insertUser(email, passwordHashed) {
+    try{
+        await pool.query("INSERT INTO users (email,password) VALUES($1,$2)", [email, passwordHashed])
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+
