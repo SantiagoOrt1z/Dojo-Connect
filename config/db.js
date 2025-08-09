44d3481da85d76a7f0efc19da39492779f4fe527
Cuantocos <santiagoortiz0609@hotmail.com>
2025-08-09 18:02:55 +0200

    Leyendo la documentacion de pg, no era necesario hacer el pool.connect() ya que al ejecutar pool.query() gestiona las conexiones automaticamente, limpie el codigo un poco

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-import {Pool} from "pg"
+import { Pool } from "pg";
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -6,12 +6,6 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
-})
+});
 
-pool.connect().then(()=>{
-    console.log("Conexion satisfactoria a la base de datos")
-}).catch((err)=>{
-    console.log("Error al conectar la base de datos", err)
-})
-
-export default pool
+export default pool;
