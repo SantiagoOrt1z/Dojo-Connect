2189f4ff208fef8253a429cbb8f548c784c97657
Cuantocos <santiagoortiz0609@hotmail.com>
2025-08-09 17:23:38 +0200

    Cree el controller del usuario, por el momento solo la funcion registerUser

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+import { createUser } from "../service/userService";
+
+export async function registerUser(req,res) {
+    try{
+        const {email, password} = req.body
+        const result = await createUser(email, password)
+        res.status(201).json({message:"Usuario creado con exito"})
+    }catch(err){
+        res.status(404).json({error:"Error al crear el usuairo"}, err.message)
+    }
+    
+}
