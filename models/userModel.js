965dbd99459b3c3b8943d4b7b287ad7959dc9fd6
Cuantocos <santiagoortiz0609@hotmail.com>
2025-08-12 02:07:46 +0200

    Correccion de varios errores, sobre todo de codigo

--- models/userModel.js
+++ models/userModel.js
@@ -1,39 +1,46 @@
-import pool from "../config/db.js"
+import pool from "../config/db.js";
 
 export async function insertUser(email, passwordHashed) {
-    try{
-        const result = await pool.query("INSERT INTO users (email,password) VALUES($1,$2) RETURNING *", [email, passwordHashed])
-        return result.rows[0]
-    }
-    catch(err){
-        console.error(err)
-    }
-}
-
-export async function getAllUsers(){
-    try{
-        const result = await pool.query("SELECT * FROM users")
-        return result.rows
-    }
-    catch(err){
-        console.error(err)
+    try {
+        const result = await pool.query(
+            "INSERT INTO users (email, password) VALUES($1, $2) RETURNING *",
+            [email, passwordHashed]
+        );
+        return result.rows[0];
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
 }
 
-export async function  getUser(email) {
-    try{
-        const result = await pool.query("SELECT * FROM users WHERE email = 1$",[email])
-        return result.rows[0]
-    }catch(err){
-        console.error(err)
+export async function getAllUsers() {
+    try {
+        const result = await pool.query("SELECT * FROM users");
+        return result.rows;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export async function getUser(email) {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+        return result.rows[0];
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
 }
 
 export async function deleteUser(email) {
-    try{
-        await pool.query("DELETE FROM users WHERE $1",[email])
-    }catch(err){
-        console.error(err)
+    try {
+        await pool.query("DELETE FROM users WHERE email = $1", [email]);
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
-    
 }
