7f86e59b7ce72e49ac045dd107575e871eb75544
Cuantocos <santiagoortiz0609@hotmail.com>
2025-08-12 02:11:25 +0200

    Algunos errores de sintaxis corregidos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 import { createUser } from "../service/userService.js";
 
-export async function registerUser(req,res) {
-    try{
-        const {email, password} = req.body
-        const result = await createUser(email, password)
-        res.status(201).json({message:"Usuario creado con exito"})
-    }catch(err){
-        res.status(404).json({error:"Error al crear el usuairo"}, err.message)
+export async function registerUser(req, res) {
+    try {
+        const { email, password } = req.body;
+        await createUser(email, password);
+        res.status(201).json({ message: "Usuario creado con éxito" });
+    } catch (err) {
+        res.status(500).json({
+            error: "Error al crear el usuario",
+            details: err.message
+        });
     }
-    
 }
