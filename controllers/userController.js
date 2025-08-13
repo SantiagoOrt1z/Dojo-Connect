472344733899833a5dddd398aff3e82ea0ea7f59
Cuantocos <santiagoortiz0609@hotmail.com>
2025-08-13 02:22:20 +0200

    Agregue las funciones editUser y la funcion deleteUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
-import { createUser } from "../service/userService.js";
+import { createUser,updateDataUser } from "../service/userService.js";
+import { deleteUser } from "../models/userModel.js";
 
 export async function registerUser(req, res) {
     try {
@@ -10,5 +11,31 @@ export async function registerUser(req, res) {
             error: "Error al crear el usuario",
             details: err.message
         });
+    }
+}
+
+export async function editUser(req,res) {
+    try{
+        const {id, email, password} = req.body
+       const result = await updateDataUser(id,email,password)
+       res.status(200).json(result)
+    }catch(err){
+        res.status(500).json({
+            error:"Error al editar el usuario",
+            details: err.message
+        })
+    }
+}
+
+export async function deleteUser(req,res) {
+    try{
+        const {email} = req.body
+       await deleteUser(email)
+       res.status(200).json({message:"Usuario eliminado correctamente"})
+    }catch(err){
+        res.status(500).json({
+            error:"Error al eliminar el usuario",
+            details: err.message
+        })
     }
 }
